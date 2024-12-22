22ea1acce61e76848c5437f5d7335cd95c568839
Maitree Thakar <maitreethakar@trentu.ca>
2024-12-22 08:24:32 -0500

    step-13

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/route.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
